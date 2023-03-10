e52fd8a2a99b0a190f4e00fda4bf2e61747698d9
ccg <aimerchenyuan@163.com>
2023-03-10 23:15:37 +0800

    add for remove electronAPI type check

---
+++ src/renderer.d.ts
@@ -0,0 +1,14 @@
+// remove editor warn about electronAPI types
+export interface IElectronAPI {
+  openFile: (callback: any) => Promise<void>,
+  saveFile: (callback: any) => Promise<void>,
+  require: (callback: any) => Promise<void>,
+  toggleView: (callback: any) => Promise<void>,
+  setFilePath: (filePath: string) => Promise<void>,
+}
+
+declare global {
+  interface Window {
+    electronAPI: IElectronAPI
+  }
+}
