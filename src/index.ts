cce8c3becf8b51774fe159849fa73d9570bf5d22
ccg <aimerchenyuan@163.com>
2023-05-28 20:11:16 +0800

    close devtool

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ const createWindow = (): void => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
   win = mainWindow
 }
 
